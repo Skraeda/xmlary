17b38839f9234d91c29b178c03e07e0bf19c1fac
Gunnar Örn Baldursson <gunnar@sjukraskra.is>
2020-06-12 14:14:57 +0000

    add: readernode

--- src/XmlReaderNode.php
+++ src/XmlReaderNode.php
@@ -2,6 +2,9 @@
 
 namespace Skraeda\Xmlary;
 
+use DOMNode;
+use Skraeda\Xmlary\Contracts\XmlReaderNodeConfigurationContract;
+
 /**
  * Single node for XmlReader.
  *
@@ -9,4 +12,147 @@ namespace Skraeda\Xmlary;
  */
 class XmlReaderNode
 {
+    /**
+     * DOMNode.
+     *
+     * @var \DOMNode
+     */
+    protected $node;
+
+    /**
+     * Child nodes.
+     *
+     * @var array
+     */
+    protected $children = [];
+
+    /**
+     * Configuration contract.
+     *
+     * @var \Skraeda\Xmlary\Contracts\XmlReaderNodeConfigurationContract
+     */
+    protected $config;
+
+    /**
+     * Value of the node
+     *
+     * @var mixed
+     */
+    protected $value;
+
+    /**
+     * Constructor.
+     *
+     * @param \DOMNode $node
+     * @param \Skraeda\Xmlary\Contracts\XmlReaderNodeConfigurationContract|null $config
+     */
+    public function __construct(DOMNode $node, ?XmlReaderNodeConfigurationContract $config = null)
+    {
+        $this->node = $node;
+        $this->config = $config ?? new XmlReaderNodeConfiguration;
+    }
+
+    /**
+     * Get underlying DOMNode.
+     *
+     * @return \DOMNode
+     */
+    public function getNode(): DOMNode
+    {
+        return $this->node;
+    }
+
+    /**
+     * Get configuration instance.
+     *
+     * @return \Skraeda\Xmlary\Contracts\XmlReaderNodeConfigurationContract
+     */
+    public function getConfiguration(): XmlReaderNodeConfigurationContract
+    {
+        return $this->config;
+    }
+
+    /**
+     * Get the data value of this node.
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->config->convert($this->value);
+    }
+
+    /**
+     * Set the node value.
+     *
+     * @param mixed $value
+     * @return self
+     */
+    public function setValue($value): self
+    {
+        $this->value = $value;
+        
+        return $this;
+    }
+
+    /**
+     * Get DOMNode attributes as key => value array.
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        $attributes = [];
+        foreach ($this->node->attributes as $attribute) {
+            $attributes[$attribute->nodeName] = $attribute->value;
+        }
+        return $attributes;
+    }
+
+    /**
+     * Get the name of this node.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->config->rename($this->node->nodeName);
+    }
+
+    /**
+     * Add a reader node as child to this one.
+     *
+     * @param XmlReaderNode $node
+     * @return self
+     */
+    public function addChild(XmlReaderNode $node): self
+    {
+        if (!array_key_exists($node->getName(), $this->children)) {
+            $this->children[$node->getName()] = [];
+        }
+
+        $this->children[$node->getName()][] = $node;
+        
+        return $this;
+    }
+
+    /**
+     * Get children as grouped by name values.
+     *
+     * @return array
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * Determine if this is a leaf node.
+     *
+     * @return boolean
+     */
+    public function isLeaf(): bool
+    {
+        return count($this->children) === 0;
+    }
 }
---
+++ tests/XmlReaderNodeTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Skraeda\Xmlary\Tests;
+
+use DOMAttr;
+use DOMDocument;
+use DOMElement;
+use DOMNode;
+use DOMText;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Skraeda\Xmlary\Contracts\XmlReaderNodeConfigurationContract;
+use Skraeda\Xmlary\XmlReaderNode;
+use Skraeda\Xmlary\XmlReaderNodeConfiguration;
+
+/**
+ * Unit tests for \Skraeda\Xmlary\XmlReaderNode
+ *
+ * @author Gunnar Örn Baldursson <gunnar@sjukraskra.is>
+ */
+class XmlReaderNodeTest extends TestCase
+{
+    /**
+     * XML writer config mock.
+     *
+     * @var \Skraeda\Xmlary\Contracts\XmlReaderNodeConfigurationContract
+     */
+    protected $config;
+
+    /**
+     * Setup the test cases.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->config = Mockery::mock(XmlReaderNodeConfigurationContract::class);
+    }
+
+    /** @test */
+    public function isThereAnySyntaxError()
+    {
+        $this->assertTrue(is_object(new XmlReaderNode(Mockery::mock(DOMNode::class))));
+    }
+
+    /** @test */
+    public function itHasConfiguration()
+    {
+        $node = new XmlReaderNode(Mockery::mock(DOMNode::class), $this->config);
+        $this->assertEquals($this->config, $node->getConfiguration());
+    }
+
+    /** @test */
+    public function itDefaultsToTheNameOfTheNode()
+    {
+        $node = new XmlReaderNode(new DOMElement('abc'));
+        $this->assertEquals('abc', $node->getName());
+        $this->assertEquals('abc', $node->getNode()->nodeName);
+    }
+
+    /** @test */
+    public function itUsesConfigForRenameIfPresent()
+    {
+        $node = new XmlReaderNode(new DOMElement('abc'), new XmlReaderNodeConfiguration('newname'));
+        $this->assertEquals('newname', $node->getName());
+        $this->assertEquals('abc', $node->getNode()->nodeName);
+    }
+
+    /** @test */
+    public function itReturnsSetValueByDefault()
+    {
+        $node = new XmlReaderNode(new DOMElement('abc'));
+        $node->setValue('value');
+        $this->assertEquals('value', $node->getValue());
+    }
+
+    /** @test */
+    public function itReturnsSetValueByConverterIfPresent()
+    {
+        $node = new XmlReaderNode(new DOMElement('abc'), new XmlReaderNodeConfiguration(null, false, function ($oldValue) {
+            return $oldValue.'edited';
+        }));
+        $node->setValue('value');
+        $this->assertEquals('valueedited', $node->getValue());
+    }
+
+    /** @test */
+    public function itReturnsNodeAttributesAsKeyValue()
+    {
+        $dom = new DOMDocument;
+        $el = $dom->createElement('name');
+        $el->appendChild(new DOMAttr('key', 'value'));
+        $node = new XmlReaderNode($el);
+        $attr = $node->getAttributes();
+        $this->assertEquals('value', $attr['key']);
+    }
+
+    /** @test */
+    public function itCanAddChildren()
+    {
+        $node = new XmlReaderNode(new DOMElement('abc'));
+        $node->addChild(new XmlReaderNode(new DOMElement('def')));
+        $this->assertCount(1, $node->getChildren());
+        $this->assertCount(1, $node->getChildren()['def']);
+    }
+}
